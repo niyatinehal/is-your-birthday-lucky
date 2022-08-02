42d2e001bab2e437d8c082684b44ac3cad0c685f
Niyati Nehal <niyatinehal58@gmail.com>
2022-08-03 02:32:53 +0530

    added style.css

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const birthdate=document.querySelector("#birthdate");
 const luckyNumber=document.querySelector("#lucky-number");
 const checkBirthdate= document.querySelector("#check");
+const outerText= document.querySelector("#output");
 
-checkBirthdate.addEventListener('click', function getValues(){
-    console.log(birthdate.value, luckyNumber.value);
-})
+
+function calculateSum(dob){
+    dob=dob.replaceAll("-","");
+    let sum=0;
+    for(let i=0; i<dob.length; i++)
+    {
+        sum= sum+ Number(dob.charAt(i));
+
+    }return sum;
+}
+
+function luckyBirthdateOrNot(sum, luckyNumber){
+   if(sum%luckyNumber==0){
+    outerText.innerText="Yay! Your birthdate is lucky!";
+   }else{
+   outerText.innerText="Guess you will have to build your own luck!! All the best!";
+   }
+}
+
+function checkBirthdateIsLucky(){
+    const dob= birthdate.value;
+    const sum= calculateSum(dob);
+    luckyBirthdateOrNot(sum, luckyNumber.value);
+}
+
+checkBirthdate.addEventListener('click', checkBirthdateIsLucky)
